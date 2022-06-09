5cb6362f208e21974353cd0be7a1b621864f4383
Yassine B <yassine60160@gmail.com>
2022-06-09 16:26:03 +0200

    3eme commit

--- assets/app.js
+++ assets/app.js
@@ -7,8 +7,11 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-
+import 'bootstrap';
+import bsCustomFileInput from 'bs-custom-file-input';
 // start the Stimulus application
 import './bootstrap';
+
+bsCustomFileInput.init();
 //Compile new JavaScript file
 import './javascript/js.js'
