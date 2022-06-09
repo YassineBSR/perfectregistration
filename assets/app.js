8bc8ee1df2428a7415062112fcf75155cc792145
Yassine B <yassine60160@gmail.com>
2022-06-09 17:24:12 +0200

    add css

--- assets/app.js
+++ assets/app.js
@@ -4,14 +4,9 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
+import './../node_modules/bootstrap/dist/css/bootstrap.css';
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-import 'bootstrap';
-import bsCustomFileInput from 'bs-custom-file-input';
+
 // start the Stimulus application
 import './bootstrap';
-
-bsCustomFileInput.init();
-//Compile new JavaScript file
-import './javascript/js.js'
