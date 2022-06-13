86816353071e5601c19e18c21bf7b35ecc0392ce
Yassine B <yassine60160@gmail.com>
2022-06-13 13:22:28 +0200

    add front v2

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\User;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,7 +23,11 @@ class RegistrationFormType extends AbstractType
             ->add('firstname')
             ->add('email')
             ->add('phone')
-            ->add('birthdate')
+            ->add('birthdate', DateType::class, [
+                'attr' => ['class' => 'form-control js-datepicker'],
+                'widget' => 'single_text',
+                
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
