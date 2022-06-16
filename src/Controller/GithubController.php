d7f12f37704f61be02685539b8879fee03200455
Yassine B <yassine60160@gmail.com>
2022-06-16 16:49:27 +0200

    github connect with v1

---
+++ src/Controller/GithubController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GithubController extends AbstractController
+{
+    /**
+     * Link to this controller to start the "connect" process
+     *
+     * @Route("/connect/github", name="connect_github_start")
+     */
+    public function connectAction(ClientRegistry $clientRegistry)
+    {
+        // on Symfony 3.3 or lower, $clientRegistry = $this->get('knpu.oauth2.registry');
+
+        // will redirect to Facebook!
+        return $clientRegistry
+            ->getClient('github') // key used in config/packages/knpu_oauth2_client.yaml
+            ->redirect([
+                'public_profile', 'email', // the scopes you want to access
+            ]);
+    }
+
+    /**
+     * After going to Facebook, you're redirected back here
+     * because this is the "redirect_route" you configured
+     * in config/packages/knpu_oauth2_client.yaml
+     *
+     * @Route("/connect/github/check", name="connect_github_check")
+     */
+    public function connectCheckAction(Request $request, ClientRegistry $clientRegistry)
+    {
+        // ** if you want to *authenticate* the user, then
+        // leave this method blank and create a Guard authenticator
+        // (read below)
+
+        /** @var \KnpU\OAuth2ClientBundle\Client\Provider\GithubClient $client */
+        $client = $clientRegistry->getClient('github');
+
+        try {
+            // the exact class depends on which provider you're using
+            /** @var \League\OAuth2\Client\Provider\GithubUser $user */
+            $user = $client->fetchUser();
+
+            // do something with all this new power!
+            // e.g. $name = $user->getFirstName();
+            var_dump($user); die;
+            // ...
+        } catch (IdentityProviderException $e) {
+            // something went wrong!
+            // probably you should return the reason to the user
+            var_dump($e->getMessage()); die;
+        }
+    }
+}
